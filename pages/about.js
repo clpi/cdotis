d8d912a2d35d86a5832dfcad363702eedb379485
clpi <chrisp1877@gmail.com>
2021-03-12 22:32:31 -0800

    adding more info

--- pages/about.js
+++ pages/about.js
@@ -11,19 +11,20 @@ export default function About() {
         <Container>
           <Intro current="about" />
           <h1 class="text-4xl mb-8">About me</h1>
-          <div>
+          <div class="space-x-7">
             <Image src="/assets/face.jpg" width="300" height="300"
-              className="float-left rounded-full"/>
+              className="float-left px-10 mx-10 mr-20 rounded-full"/>
+            <Image src="/assets/boeing.JPG" width="400" height="300"
+              className="float-left ml-20 rounded-full"/>
             <div className="float-right align-middle">
               <p>I'm a Seattle-area native and University of Washington recent-graduate with a Bachelor's Materials Science & Engineering degree.</p>
+              <p>Currently, I'm working primarily on work related to Cloud infrastructure services such as AWS, Microsoft Azure, etc. and on <a href="https://devisa.io">Devisa</a> (a LLC startup in very early stages!)</p>.
+          <p>Although my attention is currently taken with tech/cloud projects, still a significant part of myself will always lie with the physical sciences in general and materials science in particular -- and to that respect, I have several hobby projects I am currently working on in my free time, and other aspirations which will see fruition down the road.</p>
               <br/>
             </div>
           </div>
 
 
-          <Image src="/assets/boeing.JPG" width="400" height="300"
-            className="float-right rounded-full"/>
-              <p>Currently, I'm working primarily on work related to Cloud infrastructure services such as AWS, Microsoft Azure, etc. and on <a href="https://devisa.io">Devisa</a> (a LLC startup in very early stages!)</p>.
 
           <p>Please <Link href="/contact">reach out to me</Link> if you'd like to talk!</p>
         </Container>
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
 import Intro from '../components/intro'
 import Layout from '../components/layout'
+import Image from 'next/image'
 import Container from '../components/container'
 export default function Projects() {
   return (
@@ -7,6 +8,17 @@ export default function Projects() {
       <Layout>
         <Container>
           <Intro current="projects"/>
+          <h1 class="text-4xl font-light">Projects</h1>
+          <p>Some things I'm working on...</p>
+          <ul>
+
+            <li>
+              <div>
+                <Image src="/assets/logo_icon_transparent.png" width="300" height="300" class = "float-left"/>
+                <p><a href="https://devisa.io" class="text-green-500 font-semibold text-lg">Devisa</a> -- An life tracking/programmatic planning project/startup I'm working on.</p>
+              </div>
+            </li>
+            </ul>
         </Container>
       </Layout>
 
