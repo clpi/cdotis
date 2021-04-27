185fda794ed7659b0e48a5d3e05f2396f671763a
Chris P <chrisp1877@gmail.com>
2021-04-26 18:39:06 -0700

    adding idlets link

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -25,7 +25,9 @@ export default function Projects() {
           <div class="border-l-4 border-green-300 pl-8">
       <h2 className="mb-8 text-lg font-light leading-tight tracking-tighter md:text-4xl">
         <span class="text-green-500 font-semibold">1. </span>Devisa
-      </h2><b>Life data automation</b> and centralization dashboard of the future. <a class="text-green-500 font-bold" href="https://devisa.io">Lookie here.</a> This is what I'm spending most of my time on these days (as of April 2021). I'm intending to go all the way with what I consider to be one of my more cohesive visions for a project generally, and a piece of software in particular. It's ambitious -- not in the manner in which I might come across as tooting my own horn, but rather, it's ambitious and a little bit frightening to think of myself seeing a full multi-tiered project out to completion on my own, but so far it's showing quite a bit of promise.
+      </h2>
+
+        <b>Life data automation</b> and centralization dashboard of the future. <a class="text-green-500 font-bold" href="https://devisa.io">Lookie here.</a> This is what I'm spending most of my time on these days (as of April 2021). I'm intending to go all the way with what I consider to be one of my more cohesive visions for a project generally, and a piece of software in particular. It's ambitious -- not in the manner in which I might come across as tooting my own horn, but rather, it's ambitious and a little bit frightening to think of myself seeing a full multi-tiered project out to completion on my own, but so far it's showing quite a bit of promise.
 
       <br/><br/>
         <p>In the process of developing Devisa over the last month or two (again, as of April 2021) I have learned much more than I ever would have thought possible in so short a time about systems architecture, engineering, and all that more abstract jazz, but I've also come to appreciate the zen-like machinery of working away at Flutter layouts and various JavaScript frontend peculariarities that I am inevitably exposed to working on all levels of such a large and unwieldy project. </p>
@@ -46,7 +48,17 @@ export default function Projects() {
           <div class="border-l-4 border-blue-300 pl-8 mt-8">
       <h2 className="mb-8 text-lg font-light leading-tight tracking-tighter md:text-4xl">
         <span class="text-blue-500 font-semibold">2. </span>Idlets Inc.
-      </h2><b>More info forthcoming...</b> check back later!
+      </h2>
+
+        <b>More info forthcoming...</b> check back later!
+                <Image src="/assets/icon2.png" width="300" height="300" class = "float-left"/>
+      <br/><br/>
+      <p>Resources related to Idlets:</p>
+      <ul class="ml-0 border-l-4 border-yellow-200 pl-8">
+        <li>1. <a class="text-green-500" href="https://idlets.com">Idlets website</a></li>
+        <li>2. <a class="text-green-500" href="https://blog.idlets.com">Idlets blog</a></li>
+      </ul>
+
   </div>
           <div class="border-l-4 border-red-300 pl-8 mt-8">
       <h2 className="mb-8 text-lg font-light leading-tight tracking-tighter md:text-4xl">
