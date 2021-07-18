e5819c1dcd2c729f3f02074930cf1275859c6987
Chris Pecunies <clp@clp.is>
2021-07-18 07:05:55 -0700

    updating deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import mdsvex from 'mdsvex';
 import vercel from '@sveltejs/adapter-vercel'
 import adapter from '@sveltejs/adapter-static'
 
@@ -7,6 +8,9 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
+	extensions: [
+		'.svelte', '.svx'
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
